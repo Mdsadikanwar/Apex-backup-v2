f304226a51dd3dfccd2372f3cd69f0dbf3fb8531
Mdsadikanwar <91738121+Mdsadikanwar@users.noreply.github.com>
2026-07-11 17:40:21 +0400

    Create strategies.js

---
+++ strategies.js
@@ -0,0 +1,8 @@
+function render_strategies() {
+  document.getElementById('tab-content').innerHTML = `
+    <div class="card">
+      <h3>🎯 Strategies Tab</h3>
+      <p>Yaha Strategy Banane ka option aayega</p>
+    </div>
+  `;
+}
