2657414b3267fa72328ec92436479bb6482e445e
Mdsadikanwar <91738121+Mdsadikanwar@users.noreply.github.com>
2026-07-11 17:37:49 +0400

    Add render_backtest function for backtest tab

---
+++ backtest.js
@@ -0,0 +1,8 @@
+function render_backtest() {
+  document.getElementById('tab-content').innerHTML = `
+    <div class="card">
+      <h3>📈 Backtest Tab</h3>
+      <p>Yaha Backtest Engine aayega</p>
+    </div>
+  `;
+}
