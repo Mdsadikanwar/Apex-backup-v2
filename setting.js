be7815696896318828ce19112a965e91e1199af1
Mdsadikanwar <91738121+Mdsadikanwar@users.noreply.github.com>
2026-07-11 17:39:47 +0400

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,8 @@
+function render_settings() {
+  document.getElementById('tab-content').innerHTML = `
+    <div class="card">
+      <h3>⚙️ Settings Tab</h3>
+      <p>Yaha Firebase aur Telegram Settings aayengi</p>
+    </div>
+  `;
+}
