3bfc0ccfed3b579d3b88660630bf709252caae4d
Mdsadikanwar <91738121+Mdsadikanwar@users.noreply.github.com>
2026-07-11 17:39:10 +0400

    Create logs.js

---
+++ logs.js
@@ -0,0 +1,8 @@
+function render_logs() {
+  document.getElementById('tab-content').innerHTML = `
+    <div class="card">
+      <h3>📜 Logs Tab</h3>
+      <div class="log" id="logBox"></div>
+    </div>
+  `;
+}
