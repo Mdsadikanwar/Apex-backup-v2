7f1450dcf2c5b79a4818400e0d8ae463b1c6b184
Mdsadikanwar <91738121+Mdsadikanwar@users.noreply.github.com>
2026-07-26 09:55:08 +0400

    Delete setting.js

--- setting.js
+++
@@ -1,8 +0,0 @@
-function render_settings() {
-  document.getElementById('tab-content').innerHTML = `
-    <div class="card">
-      <h3>⚙️ Settings Tab</h3>
-      <p>Yaha Firebase aur Telegram Settings aayengi</p>
-    </div>
-  `;
-}
