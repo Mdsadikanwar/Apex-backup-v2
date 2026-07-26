d419544eb207a08082f2ab941389617f6d63229b
Mdsadikanwar <91738121+Mdsadikanwar@users.noreply.github.com>
2026-07-26 09:54:37 +0400

    Delete logs.js

--- logs.js
+++
@@ -1,8 +0,0 @@
-function render_logs() {
-  document.getElementById('tab-content').innerHTML = `
-    <div class="card">
-      <h3>📜 Logs Tab</h3>
-      <div class="log" id="logBox"></div>
-    </div>
-  `;
-}
